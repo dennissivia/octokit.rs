22c47d84f383d83045f5c2798df9309e181a4f25
Dennis Sivia <dennis.sivia@gmail.com>
2019-09-01 09:41:13 +0200

    Adds delete_issue_comment and OctokitError
    Adds `delete_issue_comment` by comment id.
    Adds OctokitError to unify serde_json::Error and reqwest::Error
    handling. Currently, we completely rely on the short `description` and
    in the future we should extract all details the errors provide, like
    json-parse error position and payload.
    For reqwest errors, we will extract the error type and url, etc.

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,9 @@
 use reqwest::header::{AUTHORIZATION, CONTENT_TYPE, LINK, USER_AGENT};
 use reqwest::Response;
 use serde::{Deserialize, Serialize};
-// use serde_json::Result;
+use std::error::Error;
+use std::fmt;
+// use serde_json::{Error, Result};
 
 type ID = u64;
 type CommitSha = String;
@@ -22,58 +24,63 @@ enum OwnerType {
 }
 
 #[derive(Serialize, Deserialize, Debug)]
-pub struct Comment {
-    pub body: String,
-    // TODO create unsigned big int type
-    id: usize,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
 pub struct User {
     pub id: ID,
     pub login: String,
     pub node_id: GRID,
     // type: OwnerType,
-    site_admin: bool, // avatar_url: URI,
-                      // TODO consider a hypermedia mixin or something
-                      // gravatar_id: String,
-                      // url: URI,
-                      // html_url: URI,
-                      // followers_url: URI,
-                      // following_url: URI,
-                      // gists_url: URI,
-                      // starred_url: URI,
-                      // subscriptions_url: URI,
-                      // organizations_url: URI,
-                      // repos_url: URI,
-                      // events_url: URI,
-                      // received_events_url: URI
+    site_admin: bool,
+    // avatar_url: URI,
+    // TODO consider a hypermedia mixin or something
+    // gravatar_id: String,
+    // url: URI,
+    // html_url: URI,
+    // followers_url: URI,
+    // following_url: URI,
+    // gists_url: URI,
+    // starred_url: URI,
+    // subscriptions_url: URI,
+    // organizations_url: URI,
+    // repos_url: URI,
+    // events_url: URI,
+    // received_events_url: URI
+}
+
+/// https://developer.github.com/v3/issues/comments/#response-3
+#[derive(Serialize, Deserialize, Debug)]
+pub struct IssueComment {
+    pub id: ID,
+    pub body: String,
+    pub user: User,
+    // url: URI,
+    // html_url: URI,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
-struct Issue {
-    pub id: usize,
-    pub number: usize,
+pub struct Issue {
+    pub id: ID,
+    pub number: ID,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
-struct Repository {
+pub struct Repository {
     pub full_name: String,
 }
 
-// why put Payload in the name?
+// mod webhook_payloads {
 #[derive(Serialize, Deserialize, Debug)]
 struct CommentPayload {
     pub action: String,
     pub issue: Issue,
     pub repository: Repository,
-    pub comment: Comment,
+    pub comment: IssueComment,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 struct IssuePayload {
     pub issue: Issue,
 }
+// }
 
 // TODO create Review and ReviewComment payloads
 // struct Review
@@ -108,10 +115,57 @@ struct CreateComment {
     body: String,
 }
 
+#[derive(Debug)]
+pub struct OctokitError {
+    details: String,
+}
+
+impl OctokitError {
+    fn new(msg: &str) -> OctokitError {
+        OctokitError {
+            details: msg.to_string(),
+        }
+    }
+}
+
+impl fmt::Display for OctokitError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.details)
+    }
+}
+
+impl std::error::Error for OctokitError {
+    fn description(&self) -> &str {
+        &self.details
+    }
+}
+
+impl From<serde_json::Error> for OctokitError {
+    fn from(err: serde_json::Error) -> Self {
+        OctokitError::new(err.description())
+    }
+}
+
+impl From<reqwest::Error> for OctokitError {
+    fn from(err: reqwest::Error) -> Self {
+        OctokitError::new(err.description())
+    }
+}
+
 fn perform_get(token: &String, url: URI) -> Result<Response, reqwest::Error> {
     let client = reqwest::Client::new();
     client
         .get(&url[..])
+        .header(USER_AGENT, "Octokit/Rust v0.1.0")
+        .header(CONTENT_TYPE, "application/vnd.github.antiope-preview+json")
+        .header(AUTHORIZATION, String::from(format!("token {}", token)))
+        .send()
+}
+
+fn perform_delete(token: &String, url: URI) -> Result<Response, reqwest::Error> {
+    let client = reqwest::Client::new();
+    client
+        .delete(&url[..])
         .header(USER_AGENT, "Octokit/Rust v0.1.0")
         .header(CONTENT_TYPE, "application/vnd.github.antiope-preview+json")
         .header(AUTHORIZATION, String::from(format!("token {}", token)))
@@ -137,12 +191,29 @@ fn perform_post<T: Serialize>(
     result
 }
 
+/// DELETE /repos/:owner/:repo/issues/comments/:comment_id
+pub fn delete_issue_comment(token: &String, nwo: &NameWithOwner, comment_number: ID) {
+    let url: String = format!(
+        "https://api.github.com/repos/{}/issues/comments/{}",
+        nwo, comment_number
+    );
+    let result = perform_delete(token, url);
+    match result {
+        Ok(_) => {
+            println! {"DELETE succeeded"};
+        }
+        Err(err) => {
+            println!("DELETE failed {:}", err);
+        }
+    }
+}
+
 pub fn create_issue_comment(
     token: &String,
-    issue_number: &usize,
+    issue_number: ID,
     repo_name: &String,
     message: String,
-) -> Result<String, serde_json::error::Error> {
+) -> Result<IssueComment, OctokitError> {
     let new_comment = CreateComment { body: message };
     let url: String = format!(
         "https://api.github.com/repos/{}/issues/{}/comments",
@@ -151,16 +222,20 @@ pub fn create_issue_comment(
 
     let res = perform_post(&token, url, &new_comment);
     match res {
-        Ok(good_result) => {
-            println!("Request failed {:?}", good_result);
+        Ok(mut response) => {
+            println!("Request succeeded {:?}", response);
+            let created_comment: IssueComment = response.json().expect("JSON parse failed");
+            println!("{:?}", created_comment);
+            return Ok(created_comment);
         }
         Err(err) => {
             println!("Request failed {:?}", err);
+            return Err(OctokitError::from(err));
         }
     }
-    Ok(String::from("Success"))
 }
 
+// Status: TODO
 /// GET /repos/:owner/:repo/pulls/:pull_number/comments
 pub fn get_review_comments(
     token: &String,
@@ -175,10 +250,21 @@ pub fn get_review_comments(
     None
 }
 
-fn get_pull_requests(token: &String, nwo: String) {}
+// Status: TODO
+fn get_issue_batch(token: &String) {}
 
+// Status: TODO
+pub fn get_all_issues(token: &String) {
+    //TODO paginate over all issues later
+    get_issue_batch(&token);
+}
+
+// Status: TODO
+fn get_pull_requests(token: &String, nwo: &String) {}
+
+// Status: WIP (partially done)
 // GET /repos/:owner/:repo/pulls/comments
-pub fn get_all_review_comments(token: &String, nwo: NameWithOwner) -> Option<Vec<ReviewComment>> {
+pub fn get_all_review_comments(token: &String, nwo: &NameWithOwner) -> Option<Vec<ReviewComment>> {
     let url: String = format!("https://api.github.com/repos/{}/comments", nwo);
     let result = perform_get(&token, url);
 
@@ -198,13 +284,6 @@ pub fn get_all_review_comments(token: &String, nwo: NameWithOwner) -> Option<Vec
             None
         }
     }
-}
-
-fn get_issue_batch(token: &String) {}
-
-pub fn get_all_issues(token: &String) {
-    //TODO paginate over all issues later
-    get_issue_batch(&token);
 }
 
 #[cfg(test)]
